6b0f87f8827eda13b93c2d76d46c1f69046baf06
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-02-28 00:25:51 +0800

    feat(core): wire AppDelegate to bootstrap MenuBar, hotkey, log watcher, iCloud, polling on launch

--- Sources/ClaudeUsage/App/AppDelegate.swift
+++ Sources/ClaudeUsage/App/AppDelegate.swift
@@ -4,6 +4,35 @@ import Foundation
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory) // suppress Dock icon
+
+        // setup menu bar
+        MenuBarManager.shared.setup()
+
+        // request notification permission once
+        NotificationManager.shared.requestPermission()
+
+        // register global hotkey
+        let config = ConfigManager.shared.load()
+        if config.hotkey.enabled {
+            HotkeyManager.shared.register(config: config.hotkey)
+        }
+
+        // start log watcher
+        ClaudeCodeLogParser.shared.startWatching()
+
+        // start iCloud metadata query
+        if config.iCloudSync.enabled {
+            iCloudSyncManager.shared.startMetadataQuery(config: config.iCloudSync)
+        }
+
+        // start polling
+        Task { @MainActor in
+            PollingService.shared.start(config: config)
+        }
     }
-    func applicationWillTerminate(_ notification: Notification) {}
+
+    func applicationWillTerminate(_ notification: Notification) {
+        HotkeyManager.shared.unregister()
+        PollingService.shared.stop()
+    }
 }
